ad1ba24d7b6ec0f27dbbaa7dc76eb200d067d57b
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-28 12:29:24 -0300

    Pegando folha de ponto

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import requests
 
 POST_LOGIN_URL = "https://www.sdgc.com.br/sdgc/login.php"
-REQUEST_URL = "https://www.sdgc.com.br/sdgc/frequencia/lancarponto.php"
+PESQUISA_URL = "https://sdgc.com.br/sdgc/frequencia/ponto?id=ponto"
+PONTO_URL = "https://www.sdgc.com.br/sdgc/frequencia/lancarponto"
 
 payload={'nome_usuario': "pelisangela",
          'senha_usuario': 'daiana',
          'alterarSenha': '',
          'verifica': '1'}
+
+payload1 = {'organiza': 'nome_info',
+            'nome': '',
+            'matricula': '27330',
+            'cpf': '',
+            'valor_mes_ano_final': '08-2018',
+            'valor_mes_ano_inicial': '08-2018',
+            'pesquisa': '1',
+            'enviou': '1'
+    }
 
 payload2={'cpf': "11666683710",
           'pass': "",
@@ -15,12 +26,15 @@ payload2={'cpf': "11666683710",
           'biometrico': "sim",
           'oco': "nao",
           'valor_mes_ano_inicial': "08-2018",
-          'valor_mes_ano_final': "08-2018"}
+          'valor_mes_ano_final': "08-2018",
+          'Submit2': 'Confirmar'}
 
 with requests.Session() as session:
      response = session.post(POST_LOGIN_URL, data=payload)
-     #print(response.text)
-     #response = session.post(REQUEST_URL, data=payload2)
-     #print(response.text)
-     response = session.get("https://sdgc.com.br/sdgc/frequencia/ponto")
+     response = session.get(PESQUISA_URL)
+     response = session.post(PESQUISA_URL, data=payload1)
+     response = session.post(PONTO_URL, data=payload2)
      print(response.text)
+     
+    
+    
