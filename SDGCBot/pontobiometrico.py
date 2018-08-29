40dd859fbd6a8d63d3ff0a7c49579ff4714b4586
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-29 17:13:10 -0300

    pontobiometrico

---
+++ SDGCBot/pontobiometrico.py
@@ -0,0 +1,40 @@
+import requests
+
+POST_LOGIN_URL = "https://www.sdgc.com.br/sdgc/login.php"
+PESQUISA_URL = "https://sdgc.com.br/sdgc/frequencia/ponto?id=ponto"
+PONTO_URL = "https://www.sdgc.com.br/sdgc/frequencia/lancarponto"
+
+
+def requisita(parametros):
+
+    dadoslogin = {'nome_usuario': parametros['login'],
+                   'senha_usuario': parametros['senha'],
+                   'alterarSenha': '',
+                   'verifica': '1'}
+
+    dadospesquisa = {'organiza': 'nome_info',
+                        'nome': '',
+                        'matricula': parametros['matricula'],
+                        'cpf': '',
+                        'valor_mes_ano_final': parametros['datafinal'],
+                        'valor_mes_ano_inicial': parametros['datainicial'],
+                        'pesquisa': '1',
+                        'enviou': '1'}
+
+    dadosfolha = {'cpf': parametros['cpf'],
+                'pass': "",
+                'exibir': "1",
+                'busca_sub_sec': "1",
+                'biometrico': "sim",
+                'oco': "nao",
+                'valor_mes_ano_inicial':  parametros['datafinal'],
+                'valor_mes_ano_final': parametros['datainicial'],
+                'Submit2': 'Confirmar'}
+
+    with requests.Session() as session:
+        response = session.post(POST_LOGIN_URL, data=dadoslogin)
+        response = session.get(PESQUISA_URL)
+        response = session.post(PESQUISA_URL, data=dadospesquisa)
+        response = session.post(PONTO_URL, data=dadosfolha)
+        html_str = response.text
+    return html_str
--- main.py
+++ main.py
@@ -1,40 +1,12 @@
-import requests
+from SDGCBot import pontobiometrico
 
-POST_LOGIN_URL = "https://www.sdgc.com.br/sdgc/login.php"
-PESQUISA_URL = "https://sdgc.com.br/sdgc/frequencia/ponto?id=ponto"
-PONTO_URL = "https://www.sdgc.com.br/sdgc/frequencia/lancarponto"
+parametros_req = {
+    'login': 'pelisangela',
+    'senha': 'daiana',
+    'matricula': '27330',
+    'cpf': '11666683710',
+    'datafinal': '08-2018',
+    'datainicial': '08-2018'
+}
 
-payload={'nome_usuario': "pelisangela",
-         'senha_usuario': 'daiana',
-         'alterarSenha': '',
-         'verifica': '1'}
-
-payload1 = {'organiza': 'nome_info',
-            'nome': '',
-            'matricula': '27330',
-            'cpf': '',
-            'valor_mes_ano_final': '08-2018',
-            'valor_mes_ano_inicial': '08-2018',
-            'pesquisa': '1',
-            'enviou': '1'
-    }
-
-payload2={'cpf': "11666683710",
-          'pass': "",
-          'exibir': "1",
-          'busca_sub_sec': "1",
-          'biometrico': "sim",
-          'oco': "nao",
-          'valor_mes_ano_inicial': "08-2018",
-          'valor_mes_ano_final': "08-2018",
-          'Submit2': 'Confirmar'}
-
-with requests.Session() as session:
-     response = session.post(POST_LOGIN_URL, data=payload)
-     response = session.get(PESQUISA_URL)
-     response = session.post(PESQUISA_URL, data=payload1)
-     response = session.post(PONTO_URL, data=payload2)
-     print(response.text)
-     
-    
-    
+html_str = pontobiometrico.requisita(parametros_req)
