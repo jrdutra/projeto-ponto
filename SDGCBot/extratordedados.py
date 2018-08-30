de8e7c84661e20580645b33bdc9851a18798f378
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-30 12:43:47 -0300

    Extrator de Dados Concluido

--- SDGCBot/extratordedados.py
+++ SDGCBot/extratordedados.py
@@ -11,5 +11,15 @@ def extrair(html_str):
         dados.append(dado)
     dados.remove("<sub>CPF</sub>")
     dados.remove("<sub>NOME</sub>")
-    return dados
+    tabela = []
+    linha = []
+    controle = 0
+    for i in range(0, len(dados)):
+        linha.append(dados[i])
+        controle += 1
+        if controle > 11:
+            controle = 0
+            tabela.append(linha)
+            linha = []
+    return tabela
 
