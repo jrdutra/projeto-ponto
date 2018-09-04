7326547a6e7a20156cfc66ae5b42835efc398607
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-04 16:32:44 -0300

    sd

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from SDGCBot import feriado
 # ================
 # ENTRADA DE DADOS JOAO
 # ================
-mes = "08"
+mes = "09"
 ano = "2018"
-cpf = "11269350773"
-matricula = "38578"
+cpf = "11666683710"
+matricula = "27330"
 cargahorria = 6
 login = "-"
 senha = "-"
@@ -36,39 +36,4 @@ for i in range(0, len(tabela)):
 
 tabelaferiados = feriado.tabeladeferiados("feriados.csv")
 horas = calculadoradehoras.calcsaldomes(tabela, tabelaferiados, mes, ano, cargahorria)
-print("Saldo de João no mês " + mes + ": " + horas + "h")
-
-
-
-# ==============================================================================================
-
-# ================
-# ENTRADA DE DADOS JOAO
-# ================
-mes = "08"
-ano = "2018"
-cpf = "08581603742"
-matricula = "28142"
-cargahorria = 6
-login = "-"
-senha = "-"
-# ================
-
-datafinal = mes + '-' + ano
-datainicial = datafinal
-
-parametros_req = {
-    'login': login,
-    'senha': senha,
-    'matricula': matricula,
-    'cpf': cpf,
-    'datafinal': datafinal,
-    'datainicial': datainicial
-}
-
-html_str = pontobiometrico.requisita(parametros_req)
-tabela = extratordedados.extrair(html_str)
-
-tabelaferiados = feriado.tabeladeferiados("feriados.csv")
-horas = calculadoradehoras.calcsaldomes(tabela, tabelaferiados, mes, ano, cargahorria)
-print("Saldo de Amauri no mês " + mes + ": " + horas + "h")
+print("Saldo de: " + horas + "h")
