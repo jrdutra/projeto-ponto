37af8eaabc023ab3b638d86b6be0244b02508802
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-11 08:20:47 -0300

    Saida formatada

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from SDGCBot import pontobiometrico
 from SDGCBot import extratordedados
 from SDGCBot import calculadoradehoras
 from SDGCBot import feriado
+import os
 
 # ================
 # ENTRADA DE DADOS JOAO
@@ -30,10 +31,28 @@ parametros_req = {
 html_str = pontobiometrico.requisita(parametros_req)
 tabela = extratordedados.extrair(html_str)
 
-for i in range(0, len(tabela)):
-    print(tabela[i])
+os.system('cls')
+print("---------------------------------------------------------------------------------------------------------")
+print("                                            PONTO DO MÊS " + mes)
+print("---------------------------------------------------------------------------------------------------------")
 
+
+print('{:^5}'.format('Data') + '{:{align}{width}}'.format('Dia', align='<', width='10') +
+          '{:^9}'.format('Hora 1') + '{:^9}'.format('Hora 2') + '{:^9}'.format('Hora 3') +
+          '{:^9}'.format('Hora 4') + '{:^9}'.format('Hora 5') + '{:^9}'.format('Hora 6') +
+          '{:^9}'.format('Hora 7') + '{:^9}'.format('Hora 8') +
+          '{:^9}'.format('Hora 9') + '{:^9}'.format('Hora 10'))
+
+for i in range(0, len(tabela)):
+    print('{:^5}'.format(tabela[i][0]) + '{:{align}{width}}'.format(tabela[i][1], align='<', width='10') +
+          '{:^9}'.format(tabela[i][2]) + '{:^9}'.format(tabela[i][3]) + '{:^9}'.format(tabela[i][4]) +
+          '{:^9}'.format(tabela[i][5]) + '{:^9}'.format(tabela[i][6]) + '{:^9}'.format(tabela[i][7]) +
+          '{:^9}'.format(tabela[i][8]) + '{:^9}'.format(tabela[i][9]) +
+          '{:^9}'.format(tabela[i][10]) + '{:^9}'.format(tabela[i][11]))
+print("---------------------------------------------------------------------------------------------------------")
 
 tabelaferiados = feriado.tabeladeferiados("feriados.csv")
 horas = calculadoradehoras.calcsaldomes(tabela, tabelaferiados, mes, ano, cargahorria)
 print("Saldo de: " + horas + "h")
+
+print("---------------------------------------------------------------------------------------------------------")
